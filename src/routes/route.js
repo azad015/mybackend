315c5282e1dc92ebc9e0cf605c45d21a01a380d7
azad015 <arshaz9155@gmail.com>
2022-10-18 10:21:59 +0530

    file

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,68 @@
-const express = require('express');
-const router = express.Router();
-const logger = require("../logger/logger.js")
-const util = require("../util/helper")
-const validator = require("../validator/formate.js")
-const lodash = require("lodash")
+const express = require('express')
+const router = express.Router()
 
 
-router.get('/test-me', function (req, res) {
-console.log(logger.publicName())
-console.log("curretnDate: " ,util.currentDate())
-console.log("currentMonth: ",util.currentMonth())
-console.log(util.batch())
-console.log("trimFunction: ",validator.trimCase())
-console.log("lowerCase: ",validator.smallLatter())
-console.log("upperCase: ",validator.capitalLatter())
+router.get('/test-me', function(req,res){
+    res.send('My first Api..!')
+})
+
+router.get('/movies', function(req,res){
+     let moviesData = ["KGF","Pushpa","Avengers","Money Hiest"]
+    res.send(moviesData)
+})
+
+router.get('/movie/:indexNumber', function(req,res){
+
+  let myParams = req.params.indexNumber
+  let moviesData1 = ["Rang de basanti","The shining", "Lord of the rings", "Batman begins"]
+        if(myParams<0 || myParams>=moviesData1.length){
+            return res.send("The Index Value is not found")
+        }else {
+            res.send(moviesData1[myParams])
+        }
+})
 
 
-const arr =["January","February","March","April","May","June","July","August","September","October","November","December"]
-const arr1=lodash.chunk(arr,3)
-console.log("Month: ",arr1)
+router.get("/films", function(req,res){
+    const filmsName = [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+       res.send(filmsName)
+})
 
+router.get("/films/:filmId", function(req,res){
+    let filmId = req.params.filmId
+    const filmsName = [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
 
-const arr2 = [1,3,5,7,9,11,13,15,17,19]
-const newArry = lodash.tail(arr2)
-console.log("tailFunction: ",newArry)
+     for(let i=0; i<filmsName.length; i++){
+       let index = filmsName[i]
+       let id = index.id
+       if(filmId==id){
+        return res.send(index)
+       }
+     } res.send("No movie exists with this id")
+})
 
-const myArry = [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]
-const myArry2 = lodash.union(myArry)
-console.log("unionFunction: ",myArry2)
-
-const myArry3 = [["ActionMovie","Avengers"],["horror","haunted"],["comedy","hungama2"]]
-const myArr4 = lodash.fromPairs(myArry3)
-console.log(myArr4)
-
-    res.send('My first ever api!')
-});
-module.exports = router;
-
-
-
+module.exports = router
